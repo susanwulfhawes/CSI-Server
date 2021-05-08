717b1c4ce8701c5790ae3a012e9c70f571acd2db
susanwulfhawes <62613493+susanwulfhawes@users.noreply.github.com>
2021-05-08 11:12:19 -0400

    heroku deployment

--- index.js
+++ index.js
@@ -27,28 +27,7 @@ sequelize.sync();
 app.use('/infant', infant);
 app.use('/user', user);
 app.use('/care', care);
-// app.post("/user", (req, res) => {
-//     console.log(req.body);
-//   });
-  
 
-// file upload api
-// app.post('/upload', (req, res) => {
-//     if (!req.files) {
-//         return res.status(500).send({ msg: "file is not found" })
-//     }
-//         // accessing the file
-//     const myFile = req.files.file;
-//     //  mv() method places the file inside public directory
-//     myFile.mv(`${__dirname}/public/${myFile.name}`, function (err) {
-//         if (err) {
-//             console.log(err)
-//             return res.status(500).send({ msg: "Error occured" });
-//         }
-//         // returing the response with file path and name
-//         return res.send({name: myFile.name, path: `/${myFile.name}`});
-//     });
-// })
 
 app.listen(process.env.PORT, function(){
     console.log('The app is on port 3000');
