ed7cfc0cc8860c5c821ccf1744b93a5d52dae2f3
susanwulfhawes <62613493+susanwulfhawes@users.noreply.github.com>
2021-05-09 12:24:21 -0400

    fixes to build

--- controllers/carecontroller.js
+++ controllers/carecontroller.js
@@ -1,38 +1,86 @@
 let express = require('express');
 let router = express.Router();
 const Care = require('../db').import('../models/care');
+const validateSession = require('../middleware/validate-session');
+const User = require('../db').import('../models/user');
 
-router.post('/create', function(req, res) {
+// router.post('/create', validateSession, function(req, res) {
+//     Care.create({
+//         care: req.body.care,
+//         type: req.body.type,
+//         amount: req.body.amount,
+//         userId: req.user.id
+//     })
+//     // .then(
+//             function createSuccess(care) {
+//                 res.json({
+//                     care: care,
+//                     message: "care successfully registered!",
+//                     sessionToken: token,
+//              });
+//             // //         const updateCare = {
+//             // //             infantId: infant.id
+//             // //         }
+//             // //         const query = {where: {id: req.user.id}}
+                
+//             // //         User.update(updateUser, query)
+//             // //         .then(user => res.status(200).json(user))
+//             // //         .catch(err => res.status(500).json({error:err}))  
+//             //     }
+//             // // )
+//             // // .catch(err => res.status(500).json({ error: err }));
+// // }
+// );
+
+
+router.post('/create', validateSession, function(req, res) {
     Care.create({
         care: req.body.care,
         type: req.body.type,
         amount: req.body.amount,
+        userId: req.user.id,
         time: req.body.time,
-        date: req.body.date
+        date: req.body.date,
     })
     .then(
             function createSuccess(care) {
         
                     res.json({
-                        care: care,
-                        message: "care successfully registered!",
-                    });
-                }
+                    care: care,
+                    message: "care successfully registered!",
+                });
+              }
             )
             .catch(err => res.status(500).json({ error: err }));
 });
 
-router.put('/update/:id', function(req, res) {
+// Get all cares
+router.get('/allcares', function (req, res) {
+    Care.findAll()
+    .then(cares => res.status(200).json(cares))
+    .catch(err => res.status(500).json({ error: err }));
+})
+
+router.get('/carebyid/:id', function (req, res){
+    Care.findOne({
+        where: {
+            id: req.params.id
+        }
+    })
+    .then(care => res.status(200).json(care))
+    .catch(err => res.status(500).json({ error: err }));
+})
+
+router.post('/update/:id', function(req, res) {
     const updateCare = {
         care: req.body.care,
         type: req.body.type,
         amount: req.body.amount,
-        time: req.body.time,
-        date: req.body.date
+        // time: req.body.time,
+        // date: req.body.date
     }
 
     const query = {where: {id: req.params.id}}
-
     Care.update(updateCare, query)
     .then(care => res.status(200).json(care))
     .catch(err => res.status(500).json({error:err}))
