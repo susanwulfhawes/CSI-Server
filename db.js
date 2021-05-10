ec199f647b1eb5c2366c8419cd08ffadbfeae917
susanwulfhawes <62613493+susanwulfhawes@users.noreply.github.com>
2021-05-10 09:14:05 -0400

    fixed viewer role on create; removed update and delete for viewers

--- db.js
+++ db.js
@@ -9,12 +9,12 @@ require('dotenv').config();
 
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
     dialect: 'postgres',
-    dialectOptions: {
-        ssl: {
-            require: true,
-            rejectUnauthorized: false, // very important
-          }
-      }
+    // dialectOptions: {
+    //     ssl: {
+    //         require: true,
+    //         rejectUnauthorized: false, // very important
+    //       }
+    //   }
 })
 
 
